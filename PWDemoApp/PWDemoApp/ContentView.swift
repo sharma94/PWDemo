0fb03cc9a87aabe071758fdb6a363f6aa40f014d
sharma94 <ramya.sharma.m94@gmail.com>
2021-06-13 16:22:27 +0530

    Initial commit - project setup

---
+++ PWDemoApp/PWDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PWDemoApp
+//
+//  Created by R M Sharma on 13/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PWDemoApp/PWDemoApp/PWDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PWDemoAppApp.swift
+//  PWDemoApp
+//
+//  Created by R M Sharma on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PWDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
